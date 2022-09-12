b33c10225003b1a0faef753eebee9d46f701be10
Chiranjeevisha <90556993+Chiranjeevisha@users.noreply.github.com>
2022-09-12 16:24:56 +0530

    testing anim word top

--- script.js
+++ script.js
@@ -65,68 +65,7 @@ for (var i = 0; i < btns.length; i++) {
 }
 // Filter Data End
 
-// Top-Word-Business-Animation
-var TxtType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 50;
-    this.txt = "";
-    this.tick();
-    this.isDeleting = false;
-  };
 
-  TxtType.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-    var that = this;
-    var delta = 200 - Math.random() * 1;
-
-    if (this.isDeleting) {
-      delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 0;
-    }
-
-    setTimeout(function () {
-      that.tick();
-    }, delta);
-  };
-
-  window.onload = function () {
-    var elements = document.getElementsByClassName("typewrite");
-    for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute("data-type");
-      var period = elements[i].getAttribute("data-period");
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // Inserting CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML =
-      ".typewrite > .wrap { font-size: 5vw; font-weight: bolder; z-index: 1;}";
-    document.body.appendChild(css);
-  };
-
-  // Animation Ends
          // Big Card Image Shadow
          $(document).ready(function(){
             $(".only-on-hover-1").hide();
