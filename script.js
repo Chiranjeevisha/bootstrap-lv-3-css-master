bc8cb073d5bc43cf51279e7c921b6fefb5222044
Chiranjeevisha <90556993+Chiranjeevisha@users.noreply.github.com>
2022-09-15 15:59:30 +0530

    abunatuib ciomple

--- script.js
+++ script.js
@@ -66,65 +66,65 @@ for (var i = 0; i < btns.length; i++) {
 // Filter Data End
 
       // Top-Word-Business-Animation
-      var TxtType = function (el, toRotate, period) {
+  
+      var TxtRotate = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 50;
-        this.txt = "";
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
         this.tick();
         this.isDeleting = false;
       };
-    
-      TxtType.prototype.tick = function () {
+      
+      TxtRotate.prototype.tick = function() {
         var i = this.loopNum % this.toRotate.length;
         var fullTxt = this.toRotate[i];
-    
+      
         if (this.isDeleting) {
           this.txt = fullTxt.substring(0, this.txt.length - 1);
         } else {
           this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
-    
-        this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-    
+      
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+      
         var that = this;
-        var delta = 200 - Math.random() * 1;
-    
-        if (this.isDeleting) {
-          delta /= 2;
-        }
-    
+        var delta = 300 - Math.random() * 100;
+      
+        if (this.isDeleting) { delta /= 2; }
+      
         if (!this.isDeleting && this.txt === fullTxt) {
           delta = this.period;
           this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === "") {
+        } else if (this.isDeleting && this.txt === '') {
           this.isDeleting = false;
           this.loopNum++;
-          delta = 0;
+          delta = 500;
         }
-    
-        setTimeout(function () {
+      
+        setTimeout(function() {
           that.tick();
         }, delta);
       };
-    
-      window.onload = function () {
-        var elements = document.getElementsByClassName("typewrite");
-        for (var i = 0; i < elements.length; i++) {
-          var toRotate = elements[i].getAttribute("data-type");
-          var period = elements[i].getAttribute("data-period");
+      
+      window.onload = function() {
+        var elements = document.getElementsByClassName('txt-rotate');
+        for (var i=0; i<elements.length; i++) {
+          var toRotate = elements[i].getAttribute('data-rotate');
+          var period = elements[i].getAttribute('data-period');
           if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
           }
         }
-        // Inserting CSS
+        // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML =
-          ".typewrite > .wrap { font-size: 5vw; font-weight: bolde; z-index: 1;}";
+        css.innerHTML = ".txt-rotate > .wrap {font-weight: bold; background: linear-gradient(to right, #fe506a 0%, #f99c4c 100%); -webkit-background-clip: text;  -webkit-text-fill-color: transparent; font-size: 5vw; font-weight: bolde; z-index: 1; }";
         document.body.appendChild(css);
       };
+
+ // Top-Word-Business-Animation End
     
       // Animation Ends
 
